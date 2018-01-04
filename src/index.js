fc9781610bb5de87b40de68f4b357730d5baee18
Alex Alegre <alexander.alegre@outlook.com>
2018-01-03 23:59:47 -0700

    rearrange images dir and fix props

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import * as testImgs from './icons';
+
 class WeatherIcon extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
   }
   render () {
     const {name} = this.props;
+    console.log(testImgs);
     return (
       <span>{name}</span>
     );
