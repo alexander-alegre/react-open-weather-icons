34eb29a5c92f9d69fa1e5042784da7a7c887d4cd
Alex Alegre <alexander.alegre@outlook.com>
2018-01-04 00:37:54 -0700

    remove images not being used and add reference sheet

---
+++ src/imageSelector.js
@@ -0,0 +1,43 @@
+export default imageSelector(name) {
+  let image = '';
+  switch (name) {
+    case '01d':
+      return image = '';
+    case '01n':
+      return image = ''
+    case '02d':
+      return image = '';
+    case '02n':
+      return image = ''
+    case '03d':
+      return image = '';
+    case '03n':
+      return image = ''
+    case '04d':
+      return image = '';
+    case '04n':
+      return image = ''
+    case '09d':
+      return image = '';
+    case '09n':
+      return image = ''
+    case '10d':
+      return image = '';
+    case '10n':
+      return image = ''
+    case '11d':
+      return image = '';
+    case '11n':
+      return image = ''
+    case '13d':
+      return image = '';
+    case '13n':
+      return image = ''
+    case '50d':
+      return image = '';
+    case '50n':
+      return image = ''
+    default:
+      image = '';
+  };
+}
